d914645592f39a6d5e200aa7218b60da391508b8
cipriano <ciprianogc1989@gmail.com>
2019-08-05 17:38:24 -0500

    actualizar

--- clase 0/clase_0.R
+++ clase 0/clase_0.R
@@ -15,7 +15,7 @@ a+5
 
 
 diametro <- c(12, 8.6, 9.2, 7.7, 12.9, 11.7, 9.7, 14.2,
-              1.8, 14.3, 12.5)
+              11.8, 14.3, 12.5)
 
 diametro
 # medidas de tendencia central
@@ -24,3 +24,15 @@ median(diametro)
 # medidas de dispercion
 sd(diametro)
 var(diametro)
+
+
+# boxplot (diametro) ------------------------------------------------------
+
+boxplot(diametro, horizontal=TRUE,  col="lightblue", main="diametro",
+        xlab="D (cm)")
+
+
+# importar excel ----------------------------------------------------------
+
+DB_alturas <- read.csv("C:/MCF 202-2019/Datos/alturas.csv", header= T)
+head(DB_alturas)
