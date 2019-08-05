4b11cd9ba271aca17752d389e62ac0d1f22b8132
cipriano <ciprianogc1989@gmail.com>
2019-08-05 15:35:02 -0500

    clase0R

---
+++ clase 0/clase_0.R
@@ -0,0 +1,26 @@
+# Cipriano Guerrero Cabrera
+# 05/08/2019
+# clase 0
+
+
+# # Pasos Basicos ---------------------------------------------------------
+
+2+2
+a <- 2
+a*a
+a+5
+
+
+# # importar datos --------------------------------------------------------
+
+
+diametro <- c(12, 8.6, 9.2, 7.7, 12.9, 11.7, 9.7, 14.2,
+              1.8, 14.3, 12.5)
+
+diametro
+# medidas de tendencia central
+mean(diametro)
+median(diametro)
+# medidas de dispercion
+sd(diametro)
+var(diametro)
